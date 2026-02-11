d1124d1d23f02d361d6cafc662b3382a33c6a5e3
Raoan Marcel <raoanmarceljukowski@gmail.com>
2026-02-11 00:34:13 -0300

    Configs para deploy

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
-# app.py
 import pichau
 import kabum
 import time
+import terabyte
+import mercadolivre
+
+import streamlit as st
+import subprocess
+import sys
+import os
+import warnings
+import asyncio
+import json
+import pandas as pd
+import time
+import plotly.express as px
+import io 
+
+def install_playwright():
+    if "PLAYWRIGHT_INSTALLED" not in st.session_state:
+        with st.spinner("Instalando navegador para coleta de dados..."):
+            try:
+                subprocess.run([sys.executable, "-m", "playwright", "install", "chromium"], check=True)
+                subprocess.run([sys.executable, "-m", "playwright", "install-deps"], check=True)
+                st.session_state["PLAYWRIGHT_INSTALLED"] = True
+                st.success("Navegador instalado!")
+            except Exception as e:
+                st.error(f"Erro na instalação: {e}")
+
+install_playwright()
+
 
 def iniciar_monitoramento_global(produto):
     print(f"\n🚀 INICIANDO MONITORAMENTO DE DADOS: {produto.upper()}\n")
     
-    # Executa Kabum
     kabum.buscar(produto)
     
     print("⏳ Trocando de loja...")
     time.sleep(2)
     
-    # Executa Pichau
     pichau.buscar(produto)
+
+    print("⏳ Trocando de loja...")
+    time.sleep(2)
+
+    terabyte.buscar_produtos(produto)
+
+    print("⏳ Trocando de loja...")
+    time.sleep(2)
+
+    mercadolivre.buscar_produtos(produto)
 
 if __name__ == "__main__":
     termo = input("O que deseja buscar no mercado hoje? ")
